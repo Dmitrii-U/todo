28098b5c5979d8adc9ed05ef1a964bdb8e6bc45b
  Dmitrii Uzunov <duyz@ya.ru>
2025-02-09 19:29:54 +0300

    jwt: добавлениа поддержка JWT авторизации

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+from home import urls_api_token
 from search import views as search_views
 
 urlpatterns = [
@@ -13,6 +14,7 @@ urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
+    path("api/token/", include(urls_api_token)),
 ]
 
 
